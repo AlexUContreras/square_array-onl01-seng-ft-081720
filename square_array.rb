0fcb06352ed1bdbf4df5cd49d60c5228e564e510
Alex  Contreras <alex.code29@gmail.com>
2020-08-29 11:04:35 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,5 @@
-def square_array(array)
-  new_array = []
-  array.each do |number|
-  square_number = number*number
-  new_array << square_number
-  end
-  new_array
+def square_array 
+  square_array.each do 
+  
+  
 end
